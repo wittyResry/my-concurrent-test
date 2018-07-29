d9d6aafcf4123dc967c3c6599aad6588993d427b
Qingyu Li <liqingyu.lqy@gmail.com>
2018-07-29 22:46:11 +0800

    Instance confinement

---
+++ src/main/java/com/mytest/sharing/PersonSet.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mytest.sharing;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.mytest.common.annotation.GuardedBy;
+import com.mytest.common.annotation.ThreadSafe;
+
+/**
+ * ÊµÀý·â±Õ
+ * 
+ * @author liqingyu
+ * @since 2018/07/26
+ */
+@ThreadSafe
+public class PersonSet {
+    @GuardedBy("this")
+    private final Set<Person> mySet = new HashSet<Person>();
+
+    public synchronized void addPerson(Person p) {
+        mySet.add(p);
+    }
+
+    public synchronized boolean containsPerson(Person p) {
+        return mySet.contains(p);
+    }
+
+    interface Person {
+    }
+}
